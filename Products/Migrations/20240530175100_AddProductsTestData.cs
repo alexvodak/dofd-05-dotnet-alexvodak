108bccae51cf00ed95bb5158d5bfc47018b2727e
Ghost815 <alexvodak@gmail.com>
2024-06-20 23:01:22 +0300

    initial commit

---
+++ Products/Migrations/20240530175100_AddProductsTestData.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Products.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProductsTestData : Migration
+    {
+        private const string migrationFileName = @"Products.Migrations.Scripts.20240530175100-AddProductsTestData.sql";
+
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            using (Stream stream = Assembly.GetAssembly(typeof(AddProductsTestData)).GetManifestResourceStream(migrationFileName))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                var sql = reader.ReadToEnd();
+                if (!string.IsNullOrEmpty(sql))
+                {
+                    migrationBuilder.Sql(sql);
+                }
+            }
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Products/Migrations/Scripts/20240530175100-AddProductsTestData.sql
@@ -0,0 +1,6 @@
+﻿INSERT INTO [dbo].[Products] ([Name], [Price])
+  VALUES ('Sugar',  32.0),
+       ('Sault', 19.0),
+       ('Bread', 20.0),
+       ('Butter', 62.0),
+       ('Milk', 32.0);
